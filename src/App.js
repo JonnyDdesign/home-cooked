793ac35aac2d6c1d9053365c3027b6a77a0fbf21
JonnyDdesign <jondesserres@gmail.com>
2024-07-12 13:20:33 -0700

    Imported the RecipeList component into App.js and created an array of recipes that are passed down to the RecipeList componet as a prop.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import RecipeList from './RecipeList';
+
+const recipes = [
+  {
+    title: 'Green Chile Casserole',
+    ingredients: ['10 eggs', '1/2 cup flour', '1 teaspoon baking powder', '1 pound Monterey Jack cheese', '2 cups cottage cheese', '2 4-ounce cans diced green chiles', '1/2 cup melted butter', '1/2 teaspoon salt'],
+    instructions: 'In a large bowl, beat the eggs. Add the remaining ingredients and mix well. Pour into a greased 9 x 13 inch baking dish. Bake at 350 degrees for 40 minutes.'
+  },
+  {
+    title: 'Streusel Coffee Cake',
+    ingredients: ['3/4 cups sugar', '1/4 cup sofented butter', '1 egg', '1/2 cup milk', '1.5 cups flour', '2 teaspoons baking powder', '1/2 teaspoon salt'],
+    instructions: 'Mix sugar, butter, and egg. Stir in milk. Blend dry ingredients, stir into wet mixture. Spread batter into a greased 8 x 8 inch baking dish. Sprinkle with streusel topping (streusel topping: mix 1/2 cup brown sugar (packed), 2 tablespoons flour, 2 teaspoons cinnamon, 2 tablespoons melted butter). Bake for 25-35 minutes or until a toothpick stuck in the center comes out clean.'
+  }
+];
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <RecipeList recipes={recipes} />
     </div>
   );
 }

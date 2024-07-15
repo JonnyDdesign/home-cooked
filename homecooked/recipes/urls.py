c4a520eb603185f6a992bc9055ad3eac7244e860
JonnyDdesign <jondesserres@gmail.com>
2024-07-14 23:27:36 -0700

    Added two missing forward slahes.

--- homecooked/recipes/urls.py
+++ homecooked/recipes/urls.py
@@ -3,5 +3,5 @@ from .views import RecipeList, RecipeDetail
 
 urlpatterns = [
     path('recipes/', RecipeList.as_view(), name='recipe-list'),
-    path('recipes/<int:pk>', RecipeDetail.as_view(), name='recipe-detail'),
+    path('recipes/<int:pk>/', RecipeDetail.as_view(), name='recipe-detail'),
 ]

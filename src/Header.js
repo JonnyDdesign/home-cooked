aabf8f6e9860a60755b2f1aaad6c7f5f9dda53a8
JonnyDdesign <jondesserres@gmail.com>
2024-07-15 14:28:40 -0700

    Added a CSS styling sheet for the Recipe and Header components. Adjusted CSS styling.

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <header className="App-header">
+    <header className="app-header">
       <h1>Home Cooked</h1>
     </header>
   );
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import './Recipe.css';
 
 function Recipe({ recipe }) {
   return (
-    <div>
+    <div className="recipe-card">
       <h2>{recipe.title}</h2>
       <h3>Ingredients</h3>
       <ul>
-        {Array.isArray(recipe.ingredients) ? (
-          recipe.ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))
-        ) : (
-          <li>No ingredients available</li>
-        )}
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
       </ul>
       <h3>Instructions</h3>
       <p>{recipe.instructions}</p>

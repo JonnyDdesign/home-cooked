0e926367aa535d01f880b0c7dfd0157fff57672b
JonnyDdesign <jondesserres@gmail.com>
2024-07-15 13:23:12 -0700

    Updated the serializer and model definitions to have the ingredients displayed as an array.

--- homecooked/recipes/models.py
+++ homecooked/recipes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
-    ingredients = models.TextField()
+    ingredients = models.JSONField()
     instructions = models.TextField()
 
     def __str__(self):

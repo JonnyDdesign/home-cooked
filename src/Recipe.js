5047270f7a60ea21e50ffcf23fe414c4c8273f93
JonnyDdesign <jondesserres@gmail.com>
2024-07-16 15:42:52 -0700

    Added an image, and adjusted some CSS styling.

--- src/Recipe.js
+++ src/Recipe.js
@@ -10,19 +10,21 @@ function Recipe({ recipe }) {
 
   return (
     <div className={`recipe-card ${isFlipped ? 'flipped' : ''}`} onClick={handleCardClick}>
-      <div className="recipe-card-front">
-        <h2>{recipe.title}</h2>
-        <h3>Ingredients</h3>
-        <ul>
-          {recipe.ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))}
-        </ul>
-        <h3>Instructions</h3>
-        <p>{recipe.instructions}</p>
-      </div>
-      <div className="recipe-card-back">
-        <img src={recipe.image} alt={`${recipe.title} image`} />
+      <div className="recipe-card-inner">
+        <div className="recipe-card-front">
+          <h2>{recipe.title}</h2>
+          <h3>Ingredients</h3>
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+          <h3>Instructions</h3>
+          <p>{recipe.instructions}</p>
+        </div>
+        <div className="recipe-card-back">
+          <img src={recipe.image} alt={`${recipe.title}`} />
+        </div>
       </div>
     </div>
   );

55c868938f6f465214effb708de96868c23dd66c
JonnyDdesign <jondesserres@gmail.com>
2024-07-16 15:51:31 -0700

    Updated the model to include an 'image' field. Also updated the serializer to include an 'image' field. Installed Pillow

--- homecooked/recipes/models.py
+++ homecooked/recipes/models.py
@@ -4,6 +4,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100)
     ingredients = models.JSONField()
     instructions = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
--- homecooked/recipes/serializers.py
+++ homecooked/recipes/serializers.py
@@ -4,4 +4,4 @@ from .models import Recipe
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'ingredients', 'instructions']
+        fields = ['id', 'title', 'ingredients', 'instructions', 'image']

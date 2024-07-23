9ba03bdd7ce91fdd0d85f705bfacd53a603fc95f
JonnyDdesign <jondesserres@gmail.com>
2024-07-22 20:14:18 -0700

    Updated serializers.py to handle the conversion between JSON and string for the 'ingredients' field.

--- homecooked/recipes/serializers.py
+++ homecooked/recipes/serializers.py
@@ -2,6 +2,25 @@ from rest_framework import serializers
 from .models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = serializers.JSONField()
+
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'ingredients', 'instructions', 'image']
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        recipe.set_ingredients(ingredients)
+        recipe.save()
+        return recipe
+    
+    def update(self, instance, validated_data):
+        ingredients = validated_data.pop('ingredients', None)
+        instance.title = validated_data.get('title', instance.title)
+        instance.instructions = validated_data.get('instructions', instance.instructions)
+        instance.image = validated_data.get('image', instance.image)
+        if ingredients is not None:
+            instance.set_ingredients(ingredients)
+        instance.save()
+        return instance
